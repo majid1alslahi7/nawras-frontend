dcb1974c09c2d806ce5d64a5717f79d2a8d95d34
majid1alslahi7 <majid1alslahi7@gmail.com>
2026-06-01 01:39:05 +0300

    تحديث API URL إلى nawrasb.alssemam.com

--- src/services/api.js
+++ src/services/api.js
@@ -2,19 +2,16 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-  baseURL: 'https://alssemam.live/api',
+  baseURL: 'https://nawrasb.alssemam.com/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   },
-  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
